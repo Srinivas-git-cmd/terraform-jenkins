b30a7f4d203bd9789f054ba65fb7b42cecaf7c60
Srinivas <cnu9390.g@gmail.com>
2025-12-19 16:46:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,11 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "sri" {
+  ami           = "ami-0ecb62995f68bb549" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
   }
 }
+
